949530b2c1fab99a66dad1b69d89e3dacd610d1a
benchuma12 <156671905+benchuma12@users.noreply.github.com>
2024-06-19 21:27:20 +0300

    CHRISPIAN MDUDA array work

---
+++ Chris.java
@@ -0,0 +1,14 @@
+public class Chris {
+
+    
+    public static void main(String[] args) {
+       int [] scores = new int[5];
+       scores[0]=11;
+       scores[1]=12;
+       scores[2]=1;
+       scores[3]=22;
+       scores[4]=23;
+               System.out.println(scores[3]);
+    }
+    
+}
